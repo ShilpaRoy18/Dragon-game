feb081c5ce1488ee82eb1b0afc00701487c57f71
Shilpa Roy <76759341+ShilpaRoy18@users.noreply.github.com>
2021-03-08 16:11:24 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,30 @@
+const dino = document.getElementById("dino");
+ 
+function jump(){
+    
+       if (dino.classList != "jump"){
+         dino.classList.add("jump");
+      
+    setTimeout(function () {
+        dino.classList.remove("jump");
+    },300);  
+      
+   }
+}
+
+let isAlive = setInterval(function (){
+      let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+      let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+      
+
+      if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+          alert("Game Over");
+      }
+},10) 
+document.addEventListener("keydown", function (event) {
+    jump();
+});  
+
+function start(){
+    document.getElementById("game").style.display = "block";
+}
